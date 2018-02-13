d48fc0dc8c1f94c6bd5486f5b77771d4a4769665
Feldari <35117006+Feldari@users.noreply.github.com>
2018-02-13 12:00:32 -0700

    Add files via upload

---
+++ nomatch.py
@@ -0,0 +1,72 @@
+#!/usr/bin/python3
+
+import hmac
+import sys
+
+def gen_tag(msg, key):
+    hm = hmac.new(key.encode())
+    hm.update(msg.encode())
+    return hm.hexdigest()
+
+def usage():
+    print("Usage: {} <flag> <msg> <key name>".format(sys.argv[0]))
+    print("Valid flags are -g for generate or -v to verify")
+    print("If -g msg is string if -v msg should be text file with lines")
+    print("formatted as:")
+    print("<original text string> :<hash to verify>")
+
+def generate():
+    if not sys.argv[3:]:
+        usage()
+        sys.exit(0)
+
+    msg = sys.argv[2]
+    filename = sys.argv[3] + '.key'
+
+    with open(filename) as f:
+        key = f.read()
+
+    t = gen_tag(msg, key)
+    print(msg + ':' + t)
+
+def verify():
+    if not sys.argv[3:]:
+        usage()
+        sys.exit(0)
+
+    filename = sys.argv[2] + '.txt'
+    with open(filename, 'r') as f:
+        text = f.read()
+
+    keyfile = sys.argv[3] + '.key'
+    with open(keyfile) as k:
+        key = k.read()
+
+    with open('nomatch.txt', 'a') as v:
+        v.write('Data format: <match?> :<orig message> :<given hash> '
+                ':<generated hash>\n')
+
+    for line in text.split('\n'):
+        (msg,givhash) = line.split(':')
+        genhash = gen_tag(msg, key)
+        if genhash == givhash:
+            pass
+        else:
+            match = 'NO  match'
+            output = match + " :" + msg + " :" + givhash + " :" + genhash + "\n"
+            with open('nomatch.txt', 'a') as v:
+                v.write(output)
+        
+
+if __name__ == '__main__':
+
+    if not sys.argv[3:]:
+        usage()
+        sys.exit(0)
+    elif sys.argv[1] == '-g':
+        generate()
+    elif sys.argv[1] == '-v':
+        verify()
+    else:
+        usage()
+        sys.exit(0)
